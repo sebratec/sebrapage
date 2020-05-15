12ae6f0eeccaee8e75f2bde98534c7662f18f936
Gustavo Siqueira <rsiqueira.gustavo@gmail.com>
2020-05-15 14:59:20 +0200

    Add swedish version

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){  
-	console.log(template);
 	var templateElement = document.createRange().createContextualFragment(template);
 	var place = document.querySelector("#courses");
 	var url = "http://academy.sebratec.com/api/courses";
@@ -25,11 +24,11 @@ $(document).ready(function(){
 			}
 			clone.querySelector('.bg-image').setAttribute('style','background-image: url("' + background + '")');
 			clone.querySelector('.image img').src = course["image_url"];
-			clone.querySelector('h1').innerHTML = data["name"];
-			clone.querySelector('p.lead').innerHTML = data["subtitle"].substr(0, 150);
-			clone.querySelector('p.description').innerHTML = data["short_description"].substr(0, 150);
+			clone.querySelector('h1').innerHTML = data["name"].toUpperCase();
+			clone.querySelector('h2').innerHTML = data["subtitle"];
+			clone.querySelector('p.description').innerHTML = $('<div/>').html(data["short_description"]).text();
 			// clone.querySelector('a.btn').href = course["course_url"];
 			place.parentNode.insertBefore(clone, place.nextSibling);
 		});
-	}).fail(function(jqXHR, textStatus, errorThrown) { alert('getJSON request failed! ' + textStatus); });
+	}).fail(function(jqXHR, textStatus, errorThrown) { console.log('courses request failed! ' + textStatus); });
 });
