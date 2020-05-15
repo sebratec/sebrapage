82efa5a40c674bf9806c4f0eb4910c42f611f073
Gustavo Siqueira <rsiqueira.gustavo@gmail.com>
2020-05-15 14:52:30 +0200

    Comment click here button

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	console.log(template);
 	var templateElement = document.createRange().createContextualFragment(template);
 	var place = document.querySelector("#courses");
-	var url = "http://localhost:8080/api/courses";
+	var url = "http://academy.sebratec.com/api/courses";
 	$.getJSON(url, function(json) {
 		var result = JSON.parse(json);
 		result.courses.forEach(function (course, i){
@@ -28,7 +28,7 @@ $(document).ready(function(){
 			clone.querySelector('h1').innerHTML = data["name"];
 			clone.querySelector('p.lead').innerHTML = data["subtitle"].substr(0, 150);
 			clone.querySelector('p.description').innerHTML = data["short_description"].substr(0, 150);
-			clone.querySelector('a.btn').href = course["course_url"];
+			// clone.querySelector('a.btn').href = course["course_url"];
 			place.parentNode.insertBefore(clone, place.nextSibling);
 		});
 	}).fail(function(jqXHR, textStatus, errorThrown) { alert('getJSON request failed! ' + textStatus); });
